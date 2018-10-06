cb8191714e8e512ecbb261380bd6a51542bfe2fa
Rajiv Narandas <rnarandas.gmail.com>
2018-10-07 02:27:18 +1300

    finished filtering

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -27,6 +27,33 @@ $(function() {
     });
 });
 
+// var $btns = $('.btn').click(function() {
+//   if (this.id == 'all') {
+//     $('#parent > div ').fadeIn(450);
+//   } else {
+//     var $el = $('.' + this.id).fadeIn(450);
+//     $('#parent > div ').not($el).hide();
+//   }
+//   $btns.removeClass('active2');
+//   $(this).addClass('active2');
+// })
+
+var $boxs = $("#parent > .box");
+var $btns = $(".btn").on("click", function() {
+  
+  var active = 
+    $btns.removeClass("active2")
+      .filter(this)
+      .addClass("active2")
+      .data("filter");
+  
+  $boxs
+    .hide()
+    .filter( "." + active )
+    .fadeIn(1);
+
+});
+
 
 
 
