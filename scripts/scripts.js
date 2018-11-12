06afa139fa3cced2d2512bcce7f02b33de833964
Rajiv Narandas <rnarandas.gmail.com>
2018-11-13 01:08:34 +1300

    finished site I think

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -59,8 +59,6 @@ $(function() {
         var sTarget = $(this).data('to');
         var sOffset = $(this).data('offset');
 
-        console.log(sOffset);
-
         if(typeof sOffset == "undefined"){
             sOffset = 0;
         }
@@ -72,16 +70,6 @@ $(function() {
     $('h1.animated').addClass('invisible');
 });
 
-// var $btns = $('.btn').click(function() {
-//   if (this.id == 'all') {
-//     $('#parent > div ').fadeIn(450);
-//   } else {
-//     var $el = $('.' + this.id).fadeIn(450);
-//     $('#parent > div ').not($el).hide();
-//   }
-//   $btns.removeClass('active2');
-//   $(this).addClass('active2');
-// })
 
 var $boxs = $("#parent > .box");
 var $btns = $(".btn").on("click", function() {
@@ -103,68 +91,6 @@ var $btns = $(".btn").on("click", function() {
 
 
 
-$(document).ready(function() {
-    // Test for placeholder support
-    $.support.placeholder = (function(){
-        var i = document.createElement('input');
-        return 'placeholder' in i;
-    })();
-
-    // Hide labels by default if placeholders are supported
-    if($.support.placeholder) {
-        $('.form-label').each(function(){
-            $(this).addClass('js-hide-label');
-        });  
-
-        // Code for adding/removing classes here
-        $('.form-group').find('input, textarea').on('keyup blur focus', function(e){
-            
-            // Cache our selectors
-            var $this = $(this),
-                $parent = $this.parent().find("label");
-                    
-                        switch(e.type) {
-                            case 'keyup': {
-                                 $parent.toggleClass('js-hide-label', $this.val() == '');
-                            } break;
-                            case 'blur': {
-                                if( $this.val() == '' ) {
-                    $parent.addClass('js-hide-label');
-                } else {
-                    $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
-                }
-                            } break;
-                            case 'focus': {
-                                if( $this.val() !== '' ) {
-                    $parent.removeClass('js-unhighlight-label');
-                }
-                            } break;
-                            default: break;
-                        }
-                        // previous implementation with ifs
-            /*if (e.type == 'keyup') {
-                if( $this.val() == '' ) {
-                    $parent.addClass('js-hide-label'); 
-                } else {
-                    $parent.removeClass('js-hide-label');   
-                }                     
-            } 
-            else if (e.type == 'blur') {
-                if( $this.val() == '' ) {
-                    $parent.addClass('js-hide-label');
-                } 
-                else {
-                    $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
-                }
-            } 
-            else if (e.type == 'focus') {
-                if( $this.val() !== '' ) {
-                    $parent.removeClass('js-unhighlight-label');
-                }
-            }*/
-        });
-    } 
-});
 
 
 
